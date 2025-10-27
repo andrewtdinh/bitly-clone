a9dfc487f773fd362fba3842c2fa55749f547273
Andrew Dinh <andrewdinh74@yahoo.com>
2025-10-26 17:46:31 -0700

    Modify the views and create html form

--- links/views.py
+++ links/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 
 from .models import Link
+from .forms import LinkForm
 
 
 # Create your views here.
@@ -20,5 +21,16 @@ def root_link(request, link_slug):
   return redirect(link.url)
 
 def add_link(request):
-  print(request)
-  return render(request, 'links/create.html', {})
+  if request.method == 'POST':
+    # form has data
+    form = LinkForm(request.POST)
+    if form.is_valid():
+      # process the data
+      print(form.cleaned_data)
+  else:
+    form = LinkForm()
+  
+  context = {
+    'form': form,
+  }
+  return render(request, 'links/create.html', context)
